049f7155ab1385b466833f5d8d3ab661df475b87
zaiic <zaiic@qq.com>
2023-01-06 18:16:06 +0800

    feat: implemented sending a authentication data pack to danmu host server

--- src/client/danmu_client.rs
+++ src/client/danmu_client.rs
@@ -1,21 +1,46 @@
 #![allow(dead_code)]
 
+use futures_util::{
+    stream::{SplitSink, SplitStream},
+    SinkExt, StreamExt,
+};
 use serde::{Deserialize, Serialize};
-use tokio::net::TcpStream;
+use tokio::sync::mpsc;
+use tokio::{
+    net::TcpStream,
+    sync::mpsc::{Receiver, Sender},
+};
 use tokio_tungstenite as tungstenite;
-use tungstenite::MaybeTlsStream;
+use tungstenite::{tungstenite::protocol::Message, MaybeTlsStream};
+
+use crate::utils;
 
 type WebSocketStream = tungstenite::WebSocketStream<MaybeTlsStream<TcpStream>>;
 
+pub trait Pack {
+    fn into_bytes(self) -> Vec<u8>;
+
+    fn from_bytes<'de>(data: Vec<u8>) -> Self
+    where
+        Self: Sized + Deserialize<'de>,
+    {
+        let de_json_raw: serde_json::Value = serde_json::from_slice(data.as_slice()).unwrap();
+        Self::deserialize(de_json_raw).unwrap()
+    }
+}
+
 #[derive(Debug, Default)]
 pub struct DanmuClient {
-    client: reqwest::Client,       /* Http Client */
-    auth: CookieAuth,              /* Cookie Auth */
-    room_id: u32,                  /* Room ID */
-    token: String,                 /* Token */
-    host_list: Vec<HostServer>,    /* Danmu Host Server List */
-    host_index: u8,                /* Index of Danmu Host Server Connected */
-    conn: Option<WebSocketStream>, /* Connection with Danmu Host Server */
+    client: reqwest::Client,                                 /* Http Client */
+    auth: CookieAuth,                                        /* Cookie Auth */
+    room_id: u32,                                            /* Room ID */
+    token: String,                                           /* Token */
+    host_list: Vec<HostServer>,                              /* Danmu Host Server List */
+    host_index: u8, /* Index of Danmu Host Server Connected */
+    conn_write: Option<SplitSink<WebSocketStream, Message>>, /* Connection with Danmu Host Server */
+    conn_read: Option<SplitStream<WebSocketStream>>, /* Connection with Danmu Host Server */
+    mpsc_tx: Option<Sender<Message>>, /* Channel Sender */
+    mpsc_rx: Option<Receiver<Message>>, /* Channel Receiver */
 }
 
 #[derive(Debug, Default)]
@@ -36,14 +61,14 @@ struct HostServer {
 
 #[derive(Debug)]
 pub enum DataPack {
-    Auth,          /* Authentication Pack */
-    AuthResp,      /* Authentication Response Pack */
-    HeartBeat,     /* Heart Beat Pack */
-    HeartBeatResp, /* Heart Beat Response Pack */
-    Normal,        /* Normal Pack */
+    Auth, /* Authentication Pack */
+    AuthResp, /* Authentication Response Pack */
+          // HeartBeat,     /* Heart Beat Pack */
+          // HeartBeatResp, /* Heart Beat Response Pack */
+          // Normal,        /* Normal Pack */
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Default)]
 pub struct AuthPack {
     #[serde(rename = "uid")]
     _uid: u32,
@@ -58,6 +83,8 @@ pub struct AuthPack {
     #[serde(rename = "key")]
     _key: String,
 }
+#[derive(Debug, Deserialize, Serialize, Default)]
+pub struct AuthRespPack {}
 
 impl DanmuClient {
     pub fn new(room_id: u32, auth: CookieAuth) -> Self {
@@ -73,7 +100,7 @@ impl DanmuClient {
         let resp = self
             .client
             .get(format!(
-                "https://api.live.bilibili.com/xlive/web-room/v1/index/getDanmuInfo?id={}",
+                "https://api.live.bilibili.com/xlive/web-room/v1/index/getDanmuInfo?id={}&type=0",
                 self.room_id
             ))
             .send()
@@ -87,6 +114,11 @@ impl DanmuClient {
         for obj in host_list_raw {
             self.host_list.push(HostServer::deserialize(obj).unwrap());
         }
+        // create a channel
+        let (tx, rx) = mpsc::channel(512);
+        self.mpsc_tx = Some(tx);
+        self.mpsc_rx = Some(rx);
+
         Ok(())
     }
 
@@ -94,7 +126,9 @@ impl DanmuClient {
         for (i, j) in self.host_list.iter().enumerate() {
             match tungstenite::connect_async(format!("wss://{}:{}/sub", j.host, j.wss_port)).await {
                 Ok(conn_raw) => {
-                    self.conn = Some(conn_raw.0);
+                    let (write, read) = conn_raw.0.split();
+                    self.conn_write = Some(write);
+                    self.conn_read = Some(read);
                     self.host_index = i as u8;
                     println!("Danmu Host Server Response: {:?}", conn_raw.1);
                     break;
@@ -104,12 +138,93 @@ impl DanmuClient {
         }
     }
 
+    async fn send(&mut self, data: &[u8]) {
+        self.conn_write
+            .as_mut()
+            .unwrap()
+            .send(Message::from(data))
+            .await
+            .unwrap();
+    }
+
+    async fn read(&mut self) -> Vec<u8> {
+        let res = self
+            .conn_read
+            .as_mut()
+            .unwrap()
+            .next()
+            .await
+            .unwrap()
+            .unwrap()
+            .into_data();
+
+        res
+    }
+
+    pub async fn send_auth(&mut self) -> Result<(), reqwest::Error> {
+        let auth_pack_body =
+            AuthPack::new(0, self.room_id, 3, "web".to_owned(), 2, self.token.clone());
+        let ser_body = serde_json::to_vec(&auth_pack_body).unwrap();
+        let mut auth_pack: Vec<u8> = vec![0; ser_body.len() + 16];
+
+        utils::fill_datapack_header(DataPack::Auth, auth_pack.as_mut_slice(), 1);
+        let mut offset = 16;
+        for byte in ser_body.as_slice() {
+            auth_pack[offset] = *byte;
+            offset += 1;
+        }
+
+        self.send(&auth_pack).await;
+        println!(
+            "auth_resp_pack: {:?}",
+            std::str::from_utf8(&self.read().await.as_slice()[16..])
+        );
+
+        Ok(())
+    }
+
     pub async fn connect(&mut self) -> Result<(), reqwest::Error> {
         // initialize danmu client
         self.init_client().await?;
 
         // shake hands
         self.shake_hands().await;
+
+        // send authentication pack
+        self.send_auth().await.unwrap();
+
         Ok(())
+    }
+}
+
+impl AuthPack {
+    pub fn new(
+        _uid: u32,
+        _room_id: u32,
+        _proto_ver: u8,
+        _platform: String,
+        _type: u32,
+        _key: String,
+    ) -> Self {
+        Self {
+            _uid,
+            _room_id,
+            _proto_ver,
+            _platform,
+            _type,
+            _key,
+        }
+    }
+}
+
+impl Pack for AuthPack {
+    fn into_bytes(self) -> Vec<u8> {
+        serde_json::to_vec(&self).unwrap()
+    }
+}
+
+impl Pack for AuthRespPack {
+    fn into_bytes(self) -> Vec<u8> {
+        serde_json::to_vec(&self).unwrap()
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,1 +1,52 @@
-pub fn fill_datapack_header() {}
+use crate::client::DataPack;
+
+pub fn fill_datapack_header(type_pack: DataPack, data_pack: &mut [u8], sequence: u32) {
+    let pack_len = data_pack.len() as u32;
+    for (i, byte) in pack_len.to_be_bytes().iter().enumerate() {
+        data_pack[i] = *byte;
+    }
+    let head_len: u16 = 16;
+    let ver: u16;
+    let op_code: u32;
+    let mut offset = 4;
+
+    match type_pack {
+        DataPack::Auth => {
+            (ver, op_code) = (1, 7);
+            let bs1 = head_len.to_be_bytes();
+            let bs2 = ver.to_be_bytes();
+            let bs3 = op_code.to_be_bytes();
+            let bs4 = sequence.to_be_bytes();
+
+            for byte in bs1 {
+                data_pack[offset] = byte;
+                offset += 1;
+            }
+            for byte in bs2 {
+                data_pack[offset] = byte;
+                offset += 1;
+            }
+            for byte in bs3 {
+                data_pack[offset] = byte;
+                offset += 1;
+            }
+            for byte in bs4 {
+                data_pack[offset] = byte;
+                offset += 1;
+            }
+        }
+        DataPack::AuthResp => {}
+    }
+}
+
+#[test]
+fn test_fill_datapack_header() {
+    let mut data_pack: Vec<u8> = vec![0; 32];
+    let expected: [u16; 16] = [0, 32, 16, 1, 0, 7, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0];
+    fill_datapack_header(DataPack::Auth, data_pack.as_mut_slice(), 1);
+    unsafe {
+        let raw_expected = &expected as *const u16 as *const u8;
+        let new_expected = std::slice::from_raw_parts(raw_expected.wrapping_sub(1), 32);
+        assert_eq!(new_expected, data_pack.as_slice());
+    }
+}
